2990b406693310a2e46afdcdf3916e9eaf39dba0
MatthiasKee <matthias.kee@ucalgary.ca>
2023-09-23 15:27:24 -0600

    changed foot links to icons instead of text

--- Webpage/src/App.jsx
+++ Webpage/src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "../src/Components/Home"
 import About from "../src/Components/About"
 import Skills from "../src/Components/Skills"
-import Projects from "../src/Components/Projects"
+// import Projects from "../src/Components/Projects"
 import Footer from "../src/Components/Footer"
 import "../src/Styles/reset.css"
 import "../src/Styles/app.css"
--- Webpage/src/Components/Footer.jsx
+++ Webpage/src/Components/Footer.jsx
@@ -1,13 +1,15 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
         <section className="footer">
             <div className="footer-name">Matthias Kee</div>
             <div className="footer-links">
-                <a href="mailto: matthias.kee@outlook.com" className="footer-email">EMAIL</a>
-                <a href="https://github.com/JvsonKee" target="_blank" rel="noreferrer" className="footer-github"> GITHUB</a>
-                <a href="https://www.linkedin.com/in/matthias-kee-90548a286/" target="_blank" rel="noreferrer" className="footer-linkedin">LINKEDIN</a>
+                <a href="mailto: matthias.kee@outlook.com" className="footer-email"><FontAwesomeIcon icon={faEnvelope} /></a>
+                <a href="https://github.com/JvsonKee" target="_blank" rel="noreferrer" className="footer-github"><FontAwesomeIcon icon={faGithub} /></a>
+                <a href="https://www.linkedin.com/in/matthias-kee-90548a286/" target="_blank" rel="noreferrer" className="footer-linkedin"><FontAwesomeIcon icon={faLinkedin} /></a>
             </div>
         </section>
     )
